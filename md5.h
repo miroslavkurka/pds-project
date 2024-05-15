61f6d78761959c2d9768780a3820038f894d9e90
miro <59745102+miroslavkurka@users.noreply.github.com>
2024-05-15 09:19:16 +0200

    Update md5.h

--- md5.h
+++ md5.h
@@ -5,6 +5,6 @@
 
 void md5_transform(uint32_t *state, const uint8_t *block);
 void md5_pad(uint8_t *initial_msg, uint64_t initial_len);
-void md5_hash(const uint8_t *initial_msg, size_t initial_len, uint8_t *digest);
+__host__ __device__ md5_hash(const uint8_t *initial_msg, size_t initial_len, uint8_t *digest);
 
 #endif /* MD5_H */
